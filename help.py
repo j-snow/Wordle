8cae0a6acd6e22acc4aa792e3fa18bdaa8bcbc5f
Jake Snow <j-snow@users.noreply.github.com>
2025-03-25 21:12:57 +0000

    Order by most common letters

--- help.py
+++ help.py
@@ -1,6 +1,26 @@
 import nltk
 from nltk.corpus import words
 
+word_list = words.words()
+
+words_five = [word.lower() for word in word_list if len(word) == 5]
+
+letter_frequencies = {}
+for word in words_five:
+	for i in range(0, 5):
+		letter = word[i]
+		if letter not in letter_frequencies:
+			letter_frequencies[letter] = 0
+
+		letter_frequencies[letter] = letter_frequencies[letter] + 1
+
+letter_score = sorted(letter_frequencies, key=lambda x: letter_frequencies[x], reverse=True)
+word_scores = [(word, sum([letter_score.index(letter) for letter in word]), len(set(word))) for word in words_five]
+word_scores = sorted(word_scores, key=lambda tup: tup[1])
+word_scores = sorted(word_scores, key=lambda tup: tup[2], reverse=True)
+print(word_scores[:20])
+words_five = [word_score_tuple[0] for word_score_tuple in word_scores]
+print(words_five[:20])
 
 def print_words(words):
 	num_words = len(words)
@@ -14,12 +34,6 @@ solved = False
 wordle = (None, None, None, None, None)
 ruled_out = []
 possible = []
-
-word_list = words.words()
-
-words_five = [word.lower() for word in word_list if len(word) == 5]
-print(str(len(words_five)) + ' possibilities')
-print(words_five[:20])
 
 while not solved:
 	attempt = input("Input: ")
